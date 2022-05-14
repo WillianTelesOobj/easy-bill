ad0ffee33d09fc15822bbfd81412731c9c14afaa
Willian Teles <williantelesamb@gmail.com>
2022-05-14 20:15:13 -0300

    Criando tabela MySQL

--- src/main/java/br/com/alura/oobj/easybill/EasyBillApplication.java
+++ src/main/java/br/com/alura/oobj/easybill/EasyBillApplication.java
@@ -2,9 +2,8 @@ package br.com.alura.oobj.easybill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class EasyBillApplication {
 
 	public static void main(String[] args) {
