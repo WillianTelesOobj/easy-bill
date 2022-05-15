88d3d6635704e500f5a11f6a87daf036f2786034
Willian Teles <williantelesamb@gmail.com>
2022-05-14 23:33:25 -0300

    Criando requisição POST

--- src/main/java/br/com/alura/oobj/easybill/controller/ProdutoController.java
+++ src/main/java/br/com/alura/oobj/easybill/controller/ProdutoController.java
@@ -2,14 +2,20 @@ package br.com.alura.oobj.easybill.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("produto")
+@RequestMapping("admin")
 public class ProdutoController {
 
-    @GetMapping("formulario")
+    @GetMapping("produtos/formulario")
     public String formulario() {
+        return "admin/produtos/formulario";
+    }
+
+    @PostMapping("produtos")
+    public String novo() {
         return "admin/produtos/formulario";
     }
 }
