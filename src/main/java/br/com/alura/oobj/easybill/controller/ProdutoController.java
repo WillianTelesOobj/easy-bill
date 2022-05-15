5aee97fc8cd37cccb82d25e384bf06aceec5cb41
Willian Teles <williantelesamb@gmail.com>
2022-05-14 22:21:48 -0300

    Criando formulario.html

--- src/main/java/br/com/alura/oobj/easybill/controller/ProdutoController.java
+++ src/main/java/br/com/alura/oobj/easybill/controller/ProdutoController.java
@@ -2,12 +2,14 @@ package br.com.alura.oobj.easybill.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("produto")
 public class ProdutoController {
 
-    @GetMapping("/formulario")
+    @GetMapping("formulario")
     public String formulario() {
-        return "formulario";
+        return "admin/produtos/formulario";
     }
 }
