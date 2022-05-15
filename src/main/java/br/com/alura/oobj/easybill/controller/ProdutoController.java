bb5849a265af415270649df8cf4e8c1176b99b90
Willian Teles <williantelesamb@gmail.com>
2022-05-15 18:42:07 -0300

    Formulário Novo Produto Salvando no BD

--- src/main/java/br/com/alura/oobj/easybill/controller/ProdutoController.java
+++ src/main/java/br/com/alura/oobj/easybill/controller/ProdutoController.java
@@ -1,6 +1,9 @@
 package br.com.alura.oobj.easybill.controller;
 
 import br.com.alura.oobj.easybill.dto.RequisicaoNovoProduto;
+import br.com.alura.oobj.easybill.model.Produto;
+import br.com.alura.oobj.easybill.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,13 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("admin")
 public class ProdutoController {
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     @GetMapping("produtos/formulario")
     public String formulario() {
         return "admin/produtos/formulario";
     }
 
     @PostMapping("produtos")
-    public String novo(RequisicaoNovoProduto requisicao) {
+    public String novo(RequisicaoNovoProduto requisicaoNovoProduto) {
+
+        Produto produto = requisicaoNovoProduto.toProduto();
+        produtoRepository.save(produto);
+
         return "admin/produtos/formulario";
     }
 }
