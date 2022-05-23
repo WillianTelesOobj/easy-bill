eca8cc809d8cd63b21e5e3e576643c818b802002
Willian Teles <williantelesamb@gmail.com>
2022-05-22 21:27:42 -0300

    GET /api/produtos/{id}

--- src/main/java/br/com/alura/oobj/easybill/controller/ProdutoAPIController.java
+++ src/main/java/br/com/alura/oobj/easybill/controller/ProdutoAPIController.java
@@ -1,25 +1,42 @@
 package br.com.alura.oobj.easybill.controller;
 
 import br.com.alura.oobj.easybill.dto.DevolveProdutoDto;
+import br.com.alura.oobj.easybill.dto.RequisicaoNovoProdutoDto;
 import br.com.alura.oobj.easybill.model.Produto;
 import br.com.alura.oobj.easybill.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.alura.oobj.easybill.validator.PrecoPromocionalValidator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
+@RequestMapping("api")
 @RestController
 public class ProdutoAPIController {
 
     private ProdutoRepository produtoRepository;
+    private PrecoPromocionalValidator precoPromocionalValidator;
 
-    public ProdutoAPIController(ProdutoRepository produtoRepository){
+    public ProdutoAPIController(ProdutoRepository produtoRepository, PrecoPromocionalValidator precoPromocionalValidator){
         this.produtoRepository = produtoRepository;
+        this.precoPromocionalValidator = precoPromocionalValidator;
     }
 
-    @GetMapping("/api/produtos")
+    @GetMapping("/produtos")
     public List<DevolveProdutoDto> listagemDeProdutos(){
         List<Produto> produtos = produtoRepository.findAll();
         return DevolveProdutoDto.converter(produtos);
+    }
+
+    @GetMapping("/produtos/{id}")
+    public ResponseEntity<DevolveProdutoDto> devolveProdutoPorId(@PathVariable Long id) {
+        Optional<Produto> produto = produtoRepository.findById(id);
+        if(produto.isPresent()) return ResponseEntity.ok(new DevolveProdutoDto(produto.get()));
+        return ResponseEntity.notFound().build();
     }
 }
