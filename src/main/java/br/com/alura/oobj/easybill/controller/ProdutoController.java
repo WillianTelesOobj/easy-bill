fb3669cd499928980943a3ee4b0585f6e8ea3d5d
Willian Teles <williantelesamb@gmail.com>
2022-05-22 21:42:05 -0300

    Refatoração (delete) HomeController

--- src/main/java/br/com/alura/oobj/easybill/controller/ProdutoController.java
+++ src/main/java/br/com/alura/oobj/easybill/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import br.com.alura.oobj.easybill.model.Produto;
 import br.com.alura.oobj.easybill.repository.ProdutoRepository;
 import br.com.alura.oobj.easybill.validator.PrecoPromocionalValidator;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,15 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
-@RequestMapping("admin")
+@RequestMapping("/admin")
 public class ProdutoController {
 
     private final ProdutoRepository produtoRepository;
     private final ProdutoAPIController produtoAPIController;
     private final PrecoPromocionalValidator precoPromocionalValidator;
 
+    @GetMapping("produtos")
+    public String produtos(Model model) {
+        List<Produto> produtos = produtoRepository.findAll();
+        model.addAttribute("produtos", produtos);
+        return "admin/produtos";
+    }
 
     public ProdutoController(ProdutoRepository produtoRepository, PrecoPromocionalValidator precoPromocionalValidator, ProdutoAPIController produtoAPIController){
         this.produtoRepository = produtoRepository;
