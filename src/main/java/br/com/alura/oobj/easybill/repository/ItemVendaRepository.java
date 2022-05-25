4d1441214bdc53cf7e6e2520ae0f574159a6e9da
Willian Teles <williantelesamb@gmail.com>
2022-05-25 00:18:29 -0300

    Criação Repository Venda e ItemVenda

---
+++ src/main/java/br/com/alura/oobj/easybill/repository/ItemVendaRepository.java
@@ -0,0 +1,8 @@
+package br.com.alura.oobj.easybill.repository;
+
+import br.com.alura.oobj.easybill.model.ItemVenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemVendaRepository extends JpaRepository<ItemVenda, Long> {
+
+}
