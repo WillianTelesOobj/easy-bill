715556bac9199c2744cba0357de758cf683a5720
Willian Teles <williantelesamb@gmail.com>
2022-06-02 09:28:53 -0300

    Criacao ErroDeValidacaoHandler e MensagemDeErro

---
+++ src/main/java/br/com/alura/oobj/easybill/validator/ErroDeValidacaoHandler.java
@@ -0,0 +1,44 @@
+package br.com.alura.oobj.easybill.validator;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class ErroDeValidacaoHandler {
+
+    private MessageSource messageSource;
+
+    public ErroDeValidacaoHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<MensagemDeErro> handle(MethodArgumentNotValidException exception){
+        List<MensagemDeErro> mensagemDeErros = new ArrayList<>();
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        fieldErrors.forEach(e ->{
+            String mensagem = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+            MensagemDeErro erro = new MensagemDeErro(e.getField(), mensagem);
+            mensagemDeErros.add(erro);
+        });
+        return mensagemDeErros;
+    }
+
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public List<MensagemDeErro> handlerNotFound (NotFoundException exception){
+        List<MensagemDeErro> mensagens = new ArrayList<>();
+        mensagens.add(new MensagemDeErro("Cliente", exception.getMessage()));
+        return mensagens;
+    }
+}
---
+++ src/main/java/br/com/alura/oobj/easybill/validator/MensagemDeErro.java
@@ -0,0 +1,20 @@
+package br.com.alura.oobj.easybill.validator;
+
+public class MensagemDeErro {
+
+    private String campo;
+    private String erro;
+
+    public MensagemDeErro(String campo, String erro) {
+        this.campo = campo;
+        this.erro = erro;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public String getErro() {
+        return erro;
+    }
+}
