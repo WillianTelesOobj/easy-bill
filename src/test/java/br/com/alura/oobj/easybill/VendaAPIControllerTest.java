10664dde573dcfb3f0c1a711001a72efa99943de
Willian Teles <williantelesamb@gmail.com>
2022-06-15 01:50:59 -0300

    Testes automatizados para endpoint de insercao de Vendas da API REST - Erro 400

---
+++ src/test/java/br/com/alura/oobj/easybill/VendaAPIControllerTest.java
@@ -0,0 +1,49 @@
+package br.com.alura.oobj.easybill;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.net.URI;
+
+@RunWith(SpringRunner.class)
+@Profile("test")
+@AutoConfigureMockMvc
+@SpringBootTest
+public class VendaAPIControllerTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void deveriaDevolver400CasoDadosDeCriacaoDeVendaEstejamIncorretos() throws Exception {
+
+        URI uri = new URI("/api/vendas");
+        String json = "{\"clienteId\": 5,\"  \"itensVenda\": [\" {\"quantidade\": 6, \"produtoId\": 3},\" {\"quantidade\": 1, \"produtoId\": 1}\" ]}";
+
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post(uri)
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .is(400));
+    }
+}
